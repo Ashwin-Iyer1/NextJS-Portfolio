0cb9ae0bb7150be30dcd66782c89c652c08e0b38
Ashwin-Iyer1 <ashwiniyer06@gmail.com>
2024-10-16 20:01:19 +0100

    only run on fridays

--- final.py
+++ final.py
@@ -1,8 +1,16 @@
 from last_fm import get_top
 from coverArt import get_image
 
+import datetime
+
+#only run if friday
+def is_friday():
+    return datetime.datetime.today().weekday() == 4
+
 
 def main():
+    if(not is_friday()):
+        return
     songs = get_top(10)
     for song in songs:
         get_image(song)
