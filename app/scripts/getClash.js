a53667c3ea6d836f4f426a5b8e3aeb0ed324a059
Ashwin-Iyer1 <ashwiniyer06@gmail.com>
2024-10-17 11:44:22 +0100

    Refactor getClash.js to include node-fetch dependency

--- app/scripts/getClash.js
+++ app/scripts/getClash.js
@@ -3,26 +3,24 @@ const path = require('path');
 require('dotenv').config();
 
 const token = process.env.COC_TOKEN;
-
-const url = "https://api.clashofclans.com/v1/players/%2329YOY8UJQ"; 
+const url = "https://api.clashofclans.com/v1/players/%2329YOY8UJQ";
 
 const headers = {
     "Authorization": `Bearer ${token}`
 }
 
-const getClash = async () => {
+async function getClash() {
     try {
-    const response = await fetch(url, { headers });
-    const filePath = path.join(process.cwd(),'app', 'data', 'COC.json');
-    var data = await response.json();
-    data = JSON.stringify(data);
-    fs.writeFileSync(filePath, JSON.stringify(res, null, 2), 'utf8');
-    console.log('Clash of Clans data has been saved to COC.json');
-    }
-    catch (error) {
+        var response = await fetch(url, { headers }).then((response) => response.json());
+        const filePath = path.join(process.cwd(), 'app', 'data', 'COC.json');
+        
+        // Ensure the response is OK before proceeding
+
+        fs.writeFileSync(filePath, JSON.stringify(response, null, 2), 'utf8');
+        console.log('Clash of Clans data has been saved to COC.json');
+    } catch (error) {
         console.error('Error saving COC:', error);
     }
 }
-
 
 getClash();
