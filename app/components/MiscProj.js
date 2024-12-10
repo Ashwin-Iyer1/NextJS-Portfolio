3a43a74dc4e6d2e0022053f11ae699888fc8d189
Ashwin-Iyer1 <ashwiniyer06@gmail.com>
2024-12-10 12:50:14 +0000

    Add additional iframe to MiscProj component and adjust layout for responsiveness

--- app/components/MiscProj.js
+++ app/components/MiscProj.js
@@ -6,6 +6,7 @@ export default function MiscProj() {
     return (
     <div className={styles.container}>
         <iframe src="https://www.youtube.com/embed/kebgQLcctb4" title="Financial Derivatives in Alternative Markets (Polymarket)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/PjFANvMtrqM" title="7110A | VEX Over Under | Short Reveal" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
     );
 }
--- app/resume/page.js
+++ app/resume/page.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Bar from '../components/Bar';
+import styles from "./page.module.css";
 
 export default function Resume() {
     return (
-      <div className="Resume" style={{height: '90vh', width: '100vw'}}>
+      <div className={styles.resume} style={{height: '90vh', width: '100vw'}}>
         <Bar />
         <iframe
           src="./Ashwin_Iyer_CV.pdf"
