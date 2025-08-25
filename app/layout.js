ad992a651ebd263fb13bbf4ea415f1c3af62058a
Ashwin-Iyer1 <ashwiniyer06@gmail.com>
2025-08-25 14:50:49 -0500

    add meta

--- app/layout.js
+++ app/layout.js
@@ -14,16 +14,18 @@ export default function RootLayout({ children }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content={metadata.description} />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta
-          property="og:image"
-          content="https://ashwiniyer.com/opengraph-image.png"
-        />
-        <meta
-          name="twitter:image"
-          content="https://ashwiniyer.com/opengraph-image.png"
-        />
+        <meta property="og:url" content="https://ashwiniyer.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Ashwin Iyer's Portfolio" />
+        <meta property="og:description" content="Personal website of Ashwin Iyer, a rising sophomore at Northeastern University from Southlake, Texas." />
+        <meta property="og:image" content="https://ashwiniyer.com/opengraph-image.png" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="ashwiniyer.com" />
+        <meta property="twitter:url" content="https://ashwiniyer.com" />
+        <meta name="twitter:title" content="Ashwin Iyer's Portfolio" />
+        <meta name="twitter:description" content="Personal website of Ashwin Iyer, a rising sophomore at Northeastern University from Southlake, Texas." />
+        <meta name="twitter:image" content="https://ashwiniyer.com/opengraph-image.png" />
 
         <title>{metadata.title}</title>
         <link rel="icon" href="/favicon.ico" />
